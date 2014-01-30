67bd8997e89148d6177b31bcc901ba5247a771c1
lorello <lorenzo.salvadorini@softecspa.it>
2014-01-30 08:11:32 +0100

    Added ajax loading in Boxes page: I can get browser headers, but I'm missing app headers :-(

--- resources/assets/js/script.js
+++ resources/assets/js/script.js
@@ -2,3 +2,31 @@
     $(function(){
     });
 })(jQuery);
+
+function call1(url_tocall) {
+  jQuery.noConflict();
+  (function($) {
+    var geturl;
+    geturl = $.ajax({
+        url : url_tocall,
+        success : function (data,stato) {
+            $("#boxcontent").html(data);
+            $("#callstatus").html(stato);
+            var res = []; 
+            res = geturl.getAllResponseHeaders().split("\n"); //replace(/\n/g,"<br />");
+
+            for (i=0; i < res.length; i++) {
+              if (res[i]) {
+                if (res[i].match(/^X/)) {
+                  $("#browserheaders").append("<br />["+i+"] "+res[i]+" ");
+                }
+              }
+            }
+        },
+        error : function (richiesta,stato,errori) {
+            alert("Error occurred, call state is: "+stato);
+        }
+    }); 
+  })(jQuery);
+}
+
