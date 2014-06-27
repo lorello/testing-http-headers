dc537c0dc55c5a6949dfbc31631c4c393d685763
lorello <lorenzo.salvadorini@softecspa.it>
2014-06-27 19:58:57 +0200

    Created php file to serve contents to a reverseproxy like varnish

---
+++ web/proxy.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Simple app to test HTTP Headers and reverse-proxy behaviour
+ *
+ * For Response headers details:
+ * http://api.symfony.com/master/Symfony/Component/HttpFoundation/Response.html
+ *
+ */
+
+date_default_timezone_set('Europe/Rome');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+require __DIR__.'/../resources/config/prod.php';
+require __DIR__.'/../src/app.php';
+require __DIR__.'/../src/controllers.php';
+
+$app->run();
